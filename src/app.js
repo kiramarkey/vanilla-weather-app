81da3e0e5c8e527f4ee026b667c8a9f0dcd69a69
kiramarkey <97363207+kiramarkey@users.noreply.github.com>
2022-02-19 12:24:36 -0800

    Import Code from Draft
    index.html added header and location
    style.css added starter style from draft
    app.js added function calls

---
+++ src/app.js
@@ -0,0 +1,39 @@
+function showTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureCurrent = Math.round(response.data.main.temp);
+  let weatherCurrent = response.data.weather[0].description;
+  let todayWeather = document.querySelector("#todayWeather");
+  let todayTemp = document.querySelector("#todayTemp");
+  let locationName = response.data.name;
+  let currentCityName = document.querySelector("#yourLocation");
+  currentCityName.innerHTML = `${locationName}`;
+  todayTemp.innerHTML = `${temperatureCurrent}°C`;
+  todayWeather.innerHTML = `${weatherCurrent}`;
+}
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "808c19e6529a8e88aa3a25cb2fc5e160";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+navigator.geolocation.getCurrentPosition(showPosition);
+
+function searchLocation(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#searchBar");
+  let cityOutput = document.querySelector("#yourLocation");
+  let units = "metric";
+  let apiKey = "808c19e6529a8e88aa3a25cb2fc5e160";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
+  cityOutput.innerHTML = `${cityInput.value}`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+let form = document.querySelector("form");
+form.addEventListener("submit", searchLocation);
