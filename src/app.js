856ed6adf09d871b078bf8f348805982bd42a247
kiramarkey <97363207+kiramarkey@users.noreply.github.com>
2022-02-19 16:24:32 -0800

    Added API call for date/time

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+      hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+      minutes = `0${minutes}`;
+  }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`
+}
+
+
 function showTemperature(response) {
   console.log(response.data.main.temp);
   let temperatureCurrent = Math.round(response.data.main.temp);
   let weatherCurrent = response.data.weather[0].description;
   let humidityCurrent = Math.round(response.data.main.humidity);
   let windCurrent = Math.round(response.data.wind.speed);
+  let todayDay = document.querySelector("#todayDay");
   let todayWeather = document.querySelector("#todayWeather");
   let todayTemp = document.querySelector("#todayTemp");
   let todayHumid = document.querySelector("#todayHumid");
@@ -15,6 +32,7 @@ function showTemperature(response) {
   todayWeather.innerHTML = `${weatherCurrent}`;
   todayHumid.innerHTML = `${humidityCurrent}`;
   todayWind.innerHTML = `${windCurrent}`;
+  todayDay.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 function showPosition(position) {
